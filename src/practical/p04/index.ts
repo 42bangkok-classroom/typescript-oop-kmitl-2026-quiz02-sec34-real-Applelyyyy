b58ead2a492b344e8c7e6c79032f99524f991fc3
Applelyyyy <islandgameming@gmail.com>
2026-01-20 16:38:16 +0700

    fix 8

--- src/practical/p04/index.ts
+++ src/practical/p04/index.ts
@@ -1,73 +1,50 @@
 import axios from 'axios';
 
-interface Geo {
-  lat: string;
-  lng: string;
+interface Todos {
+  userId: number
+  id: number
+  title: string
+  completed: boolean
+}
+interface User  {
+  id: number
+  name: string
+  address: {
+  street: string
+  suite: string
+  city: string
+  zipcode: string
+  geo: {
+      lat: string
+      lng: string }
+  } | null
+  phone: string
+  website: string
 }
 
-interface Address {
-  street: string;
-  suite: string;
-  city: string;
-  zipcode: string;
-  geo: Geo;
-}
-
-interface Todo {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-}
-
-interface UserWithTodos {
-  id: number;
-  name: string;
-  address: Address | null;
-  phone: string;
-  todos: Todo[];
-}
-
-interface ApiUser {
-  id: number;
-  name: string;
-  phone: string;
-  address?: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
-}
 export async function getTodosByUserId(id:number){
-    try{
-        const [usersResponse, todosResponse] = await Promise.all([
-            axios.get<ApiUser[]>('https://jsonplaceholder.typicode.com/users'),
-            axios.get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
-        ]);
-        const users = usersResponse.data
-        const todos = todosResponse.data
-        const user = users.find(f => f.id === id)
-        let userTodos = todos.filter(ff => ff.userId === id)
-        if(!user || !user.address){
-          return "Invalid id"
-        }
-        if (!userTodos){
-          userTodos = []
-        }
-        
-        return {
-          id: user.id,
-          name: user.name,
-          address: user.address || null,
-          phone: user.phone,
-          todos: userTodos,
-        }
-    }catch(error){
-        return "Invalid id"
+  try{
+    let user_respont = await axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+    const users_data = Array.isArray(user_respont?.data) ? user_respont.data : []
+    let todo_respont = await axios.get<Todos[]>('https://jsonplaceholder.typicode.com/todos')
+    const todos_data = Array.isArray(todo_respont?.data) ? todo_respont.data : []
+
+    let filterUserById = users_data.filter(users => users.id === id)
+    let filterTodosById = todos_data.filter(todo => todo.userId === id)
+    if(filterUserById.length == 1){
+      const { id, name, address, phone } = filterUserById[0];
+      return {
+        id,
+        name,
+        address,
+        phone,
+        todos: filterTodosById
+      }
     }
+    else{
+      return "Invalid id"
+    }
+  }catch(err){
+    return []
+  }
 }
