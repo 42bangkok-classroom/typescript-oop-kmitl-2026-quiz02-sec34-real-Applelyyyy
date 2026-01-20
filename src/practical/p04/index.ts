295b5cf000dd726ec66b76a2a8e520174be3765f
Applelyyyy <islandgameming@gmail.com>
2026-01-20 14:59:36 +0700

    prac

--- src/practical/p04/index.ts
+++ src/practical/p04/index.ts
@@ -43,7 +43,7 @@ interface ApiUser {
     };
   };
 }
-export async function getTodosByUserId(id:number):Promise<UserWithTodos | string> {
+export async function getTodosByUserId(id:number){
     try{
         const [usersResponse, todosResponse] = await Promise.all([
             axios.get<ApiUser[]>('https://jsonplaceholder.typicode.com/users'),
@@ -51,28 +51,19 @@ export async function getTodosByUserId(id:number):Promise<UserWithTodos | string
         ]);
         const users = usersResponse.data
         const todos = todosResponse.data
-        const user_data = users.find(f => f.id === id)
-        if(!user_data){
-            return "Invalid id"
-        }
+        const user_data = users.filter(f => f.id === id)
         const userTodos = todos.filter(ff => ff.userId === id)
-
-        return {
-            id: user_data.id,
-            name: user_data.name,
-            address: user_data.address ? {
-                street: user_data.address.street,
-                suite: user_data.address.suite,
-                city: user_data.address.city,
-                zipcode: user_data.address.zipcode,
-                geo: {
-                    lat: user_data.address.geo.lat,
-                    lng: user_data.address.geo.lng
-                }
-            } : null,
-                phone: user_data.phone,
-                todos: userTodos
-        };
+        if(user_data.length == 1){
+          return user_data.map(filtered => ({
+            id: filtered.id,
+            name: filtered.name,
+            address: filtered.address,
+            phone: filtered.phone,
+            todos: userTodos
+          }))
+        }else{
+          return "Invalid id"
+        }
     }catch(error){
         return "Invalid id"
     }
