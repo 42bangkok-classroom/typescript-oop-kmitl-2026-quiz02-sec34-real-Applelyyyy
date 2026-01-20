476c1e8fd68ff5acf85d604a8e30513059f79964
Applelyyyy <islandgameming@gmail.com>
2026-01-20 16:58:18 +0700

    fix last dai law

--- src/practical/p04/index.ts
+++ src/practical/p04/index.ts
@@ -1,40 +1,25 @@
 import axios from 'axios';
-
+import { getPostalAddress } from '../p01';
 interface Todos {
   userId: number
   id: number
   title: string
   completed: boolean
 }
-interface User  {
-  id: number
-  name: string
-  address: {
-  street: string
-  suite: string
-  city: string
-  zipcode: string
-  geo: {
-      lat: string
-      lng: string }
-  } | []
-  phone: string
-  website: string
-}
 
 export async function getTodosByUserId(id:number){
   try{
-    let user_respont = await axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
-    const users_data = Array.isArray(user_respont?.data) ? user_respont.data : []
+    let users_data = await getPostalAddress()
     let todo_respont = await axios.get<Todos[]>('https://jsonplaceholder.typicode.com/todos')
     const todos_data = Array.isArray(todo_respont?.data) ? todo_respont.data : []
 
     let filterUserById = users_data.filter(users => users.id === id)
     let filterTodosById = todos_data.filter(todo => todo.userId === id)
     if(filterUserById.length == 1){
-      const { id, name, address, phone } = filterUserById[0]
-      const result = { id, name, address, phone, todos: filterTodosById }
-      return result
+      return {
+        ...filterUserById[0],
+        todos:filterTodosById
+      }
     }
     else{
       return "Invalid id"
